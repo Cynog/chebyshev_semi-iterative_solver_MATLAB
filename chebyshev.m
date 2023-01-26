efcfdc749c2d6245bbdef7ccb50c12d88e3dcd86
Cynog <thomasratzketr@outlook.de>
2023-01-26 21:51:12 +0100

    allow for matrices and function handles

--- chebyshev.m
+++ chebyshev.m
@@ -1,4 +1,4 @@
-function [y, flag, relres, iter, resvec] = chebyshev(Afun, b, Mfun, eigmax, eigmin, tol, maxit, x0)
+function [y, flag, relres, iter, resvec] = chebyshev(A, b, M, eigmax, eigmin, tol, maxit, x0)
     % assume tolerance will not be reached
     flag = 1;
 
@@ -6,11 +6,26 @@ function [y, flag, relres, iter, resvec] = chebyshev(Afun, b, Mfun, eigmax, eigm
     if (nargin < 6) || isempty(tol)
         tol = 1e-6;
     end
+
     if (nargin < 7) || isempty(maxit)
         maxit = 20;
     end
+
     if (nargin < 8) || isempty(x0)
-        x0 = zeros(length(b),1);
+        x0 = zeros(length(b), 1);
+    end
+
+    % check if input already is a function handle
+    if isa(A, 'function_handle')
+        Afun = A;
+    else
+        Afun = @(x) A * x;
+    end
+
+    if isa(M, 'function_handle')
+        Mfun = M;
+    else
+        Mfun = @(x) M * x;
     end
 
     % check value of tol
@@ -23,23 +38,24 @@ function [y, flag, relres, iter, resvec] = chebyshev(Afun, b, Mfun, eigmax, eigm
     normb = norm(b);
 
     % useful factors for iteration
-    om = 4*((eigmax+eigmin) / (eigmax-eigmin))^2;
+    om = 4 * ((eigmax + eigmin) / (eigmax - eigmin)) ^ 2;
     omega = 2;
-    c = 2/(eigmax + eigmin);
+    c = 2 / (eigmax + eigmin);
 
     % preallocate storage for vector of l2-residuals
-    resvec = zeros(maxit+1, 1);
+    resvec = zeros(maxit + 1, 1);
 
     % set 0-th iterate to starting vector
     y_old = x0;
 
     % calculate starting residual
     r = b - Afun(x0);
-    
+
     % check convergence for starting vector
     normr = norm(r);
     resvec(1) = normr;
-    relnormr = normr/normb;
+    relnormr = normr / normb;
+
     if relnormr <= tol % convergence
         flag = 0;
         y = x0;
@@ -54,17 +70,18 @@ function [y, flag, relres, iter, resvec] = chebyshev(Afun, b, Mfun, eigmax, eigm
     relres_min = relnormr;
     iter_min = 0;
 
-    % apply stationary method to starting residual 
+    % apply stationary method to starting residual
     z = Mfun(r);
 
     % calculate first iterate
-    y = c*z + y_old;
+    y = c * z + y_old;
 
     % check convergece for first iterate
     r = b - Afun(y);
     normr = norm(r);
     resvec(2) = normr;
-    relnormr = normr/normb;
+    relnormr = normr / normb;
+
     if relnormr <= tol % convergence
         flag = 0;
         relres = relnormr;
@@ -73,7 +90,7 @@ function [y, flag, relres, iter, resvec] = chebyshev(Afun, b, Mfun, eigmax, eigm
         return;
     end
 
-    % check first iterate for smallest relative residual 
+    % check first iterate for smallest relative residual
     if (relnormr < relres_min)
         y_min = y;
         relres_min = relnormr;
@@ -86,33 +103,35 @@ function [y, flag, relres, iter, resvec] = chebyshev(Afun, b, Mfun, eigmax, eigm
         z = Mfun(r);
 
         % update factor omega
-        omega = om/(om - omega);
+        omega = om / (om - omega);
 
         % calculate new iterate
         tmp = y;
-        y = omega*(c*z + y) + (1 - omega)*y_old;
+        y = omega * (c * z + y) + (1 - omega) * y_old;
         y_old = tmp;
 
         % calculate new residual
         r = b - Afun(y);
 
         % check convergence for new iterate
-        resvec(k+1) = norm(r);
-        relnormr = resvec(k+1)/normb;
+        resvec(k + 1) = norm(r);
+        relnormr = resvec(k + 1) / normb;
+
         if relnormr <= tol % convergence
             flag = 0;
             relres = relnormr;
             iter = k;
-            resvec = resvec(1:k+1);
+            resvec = resvec(1:k + 1);
             return;
         end
 
-        % check current iterate for smallest relative residual  
+        % check current iterate for smallest relative residual
         if (relnormr < relres_min)
             y_min = y;
             relres_min = relnormr;
             iter_min = k;
         end
+
     end
 
     % tol not reached by iteration
